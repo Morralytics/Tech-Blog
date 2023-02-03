819a6652ae6beb1b139e3dc6fc00ce21cd8b6550
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-02 21:13:08 -0800

    Had to remove commas within the env file for functioning server.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const sequelize = require('./config/connection');
 
@@ -8,7 +9,8 @@ const PORT = process.env.PORT || 3001;
 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
