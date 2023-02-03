1ea70d437400058f56c3cc0705093fcd767f089a
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-02 21:44:22 -0800

    Completted basic init of a User.

--- models/User.js
+++ models/User.js
@@ -6,7 +6,30 @@ class User extends Model {}
 
 User.init(
     {
-
+        user_id : {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true, 
+        },
+        name : {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email : {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            }
+        }
     }
 );
 
