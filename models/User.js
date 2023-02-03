9af9535d6f31d252e0053d7c77ff15d7a8526bd4
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-02 21:51:09 -0800

    Made beforeCreate async function that accepts the new user password.

--- models/User.js
+++ models/User.js
@@ -33,11 +33,12 @@ User.init(
     },
     {
         hooks: {
-            beforeCreate: (newUserData) => {
-
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
             },
-            beforeUpdate: (newUserData) => {
-
+            beforeUpdate: (updatedUserData) => {
+                
             }
         },
         sequelize,
