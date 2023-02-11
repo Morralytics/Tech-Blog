25134697d625ce7135715bbcf08d463bca63d730
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-11 00:00:22 -0800

    Made adjustments to css path from handlebar files for css operation and styled a small amount for easier readability for now.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const hbs = exphbs.create({ helpers });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
