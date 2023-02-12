f1ee27f90541bbc4bef95f9fdac162b7ab290d1f
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-11 18:19:07 -0800

    Added new blog model that will hold the post information.

---
+++ models/Blog.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {}
+
+Blog.init(
+    {
+        blog_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        post: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blog'
+    },
+);
