a12a1849a2e5b799ff154d4818056b9e20a780d6
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-11 20:15:43 -0800

    Added express session information and after encountering a few errors, the addition of more users is working.

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers')
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
+const sess = {
+    secret: "Devlog's secrets",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
