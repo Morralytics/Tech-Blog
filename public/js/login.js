8ecba399e48846d6acec0b337cdd7d238ee00a85
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-12 21:29:20 -0800

    Added signup form handeling.

--- public/js/login.js
+++ public/js/login.js
@@ -26,5 +26,17 @@ const signupForm = async (event) => {
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    
-}
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json'},
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to signup.');
+        }
+    }
+};
