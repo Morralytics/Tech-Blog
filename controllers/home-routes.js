afb5912cd561ae6e99d524b201896eb8d62c0178
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-12 18:13:43 -0800

    Added route to show all users.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
+const { User, Blog } = require('../models');
 
 router.get('/login', (req, res) => {
     res.render('login')
 });
+
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll();
+        res.status(200).json(userData)
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
 
 module.exports = router;
