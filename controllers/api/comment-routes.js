eadf1a388de069446e56ff50f1e24938b75d328b
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-13 09:38:09 -0800

    Added post route for comment creation.

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -18,3 +18,12 @@ router.get('/', async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+router.post('/', async (req,res) => {
+    try {
+        const commentData= await Comment.create(...req.body);
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -14,10 +14,7 @@ router.get('/', async (req, res) => {
 // Creates a post
 router.post('/', async (req, res) => {
     try {
-        const postData = await Post.create({
-            ...req.body,
-        });
-
+        const postData = await Post.create({...req.body,});
         res.status(200).json(postData);
     } catch (err) {
         res.status(400).json(err);
