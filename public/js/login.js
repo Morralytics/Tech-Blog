23ace93686c915e48dc32d548f93851ce94cd713
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-12 21:26:43 -0800

    Query selectors for the signup inputs.

--- public/js/login.js
+++ public/js/login.js
@@ -18,3 +18,13 @@ const loginForm = async (event) => {
         }
     }
 };
+
+const signupForm = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    
+}
