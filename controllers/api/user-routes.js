ed43d22db19798bf4ea8a68db32b1e433e2b5e3a
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-12 21:00:38 -0800

    Added logout route.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -71,9 +71,21 @@ router.post('/login', async (req, res) => {
 
             res.json({ user: userLoginData, msg: 'You are logged in!'});
         });
-        
+
     } catch (err) {
         res.status(400).json(err);
     }
-})
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
 module.exports = router;
