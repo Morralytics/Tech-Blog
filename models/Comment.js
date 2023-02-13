ac12bd07524234e9b1b0c107cc3f7672aaefdeb0
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-13 08:32:46 -0800

    Added reference to user id from comment.

--- models/Comment.js
+++ models/Comment.js
@@ -14,6 +14,13 @@ Comment.init(
         text: {
             type: DataTypes.STRING,
             allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
         }
     },
     {
