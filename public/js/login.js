e8259ac4bc236c776cae78a652c569f627333041
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-12 21:15:49 -0800

    Adding query selectors for email and password inputs.

---
+++ public/js/login.js
@@ -0,0 +1,8 @@
+const loginForm = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    
+}
