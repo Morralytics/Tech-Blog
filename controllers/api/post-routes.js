d214ee56133b506d952b511dc5290a379fa77997
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-12 18:42:27 -0800

    Added route to get all posts.

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+// Gets all posts
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll();
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
