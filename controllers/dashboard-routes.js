9634d3e16a90b0cda953f2ed637ebb9ede0bc16a
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-28 12:51:26 -0700

    Refactored some code at the main dashboard page and routed the post page form to the dashboard.

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,18 +3,15 @@ const { Post } = require('../models/Post');
 const { id_auth } = require('../utils/helpers')
 
 router.get('/', id_auth, async (req, res) => {
-    if (!req.session.loggedIn) {
-        res.redirect('/login');
-        return;
-    };
-    console.log(req.session.loggedIn);
     res.render('dashboard', {
         loggedIn: req.session.loggedIn,
     });
 })
 
 router.get('/post', id_auth, async (req, res) => {
-    res.render('post');
+    res.render('post', {
+        loggedIn: req.session.loggedIn,
+    });
 });
 
 module.exports = router;
