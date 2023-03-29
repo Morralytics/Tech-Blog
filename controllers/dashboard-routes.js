55b997c7bdbacb4039d7cba61f85f8eac77e1faa
Nick Morris <nicholasmorris2599@gmail.com>
2023-03-29 10:42:56 -0700

    Homepage now displays all posts while dashboard displays the user and their posts. There is a working onclick function that currently just logs to the console.

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
-const { Post } = require('../models');
+const { Post, User } = require('../models');
 const { id_auth } = require('../utils/helpers')
 
 router.get('/', id_auth, async (req, res) => {
+    const user = await User.findOne({
+        where: { id: req.session.user_id },
+    })
+    const userInfo = user.get({ plain: true });
+
     const postData = await Post.findAll(
         {
-            where:
-                { user_id: req.session.user_id }
+            where: { user_id: req.session.user_id },
         });
     const userPosts = postData.map((posts) =>
         posts.get({ plain: true })
@@ -15,6 +19,7 @@ router.get('/', id_auth, async (req, res) => {
     res.render('dashboard', {
         loggedIn: req.session.loggedIn,
         userPosts,
+        userInfo,
     });
 })
 
