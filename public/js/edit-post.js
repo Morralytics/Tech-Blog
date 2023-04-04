477212e859ea2ffa725b96427e2be4fc99ad6049
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-04 14:39:47 -0700

    Added remove button so it can delete the post.

---
+++ public/js/edit-post.js
@@ -0,0 +1,22 @@
+const postForm = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+
+    if (title && content) {
+        const response = await fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify({ title, content }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to submit post.');
+        }
+    }
+
+};
+
+document.querySelector('.edit-form').addEventListener('submit', postForm);
